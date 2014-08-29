18cca3e264bc06b34eed8b57d21210c30aecf5bf
sixFingers <sixfingers@MacBook-Pro-di-Edoardo.local>
2014-08-29 02:44:40 +0200

    Add Source model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828232847) do
+ActiveRecord::Schema.define(version: 20140829003544) do
+
+  create_table "sources", force: true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.string   "feed_url"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sources", ["author_id"], name: "index_sources_on_author_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
